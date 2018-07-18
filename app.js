68f9a1672f324f08ee0756a9b4da18d5e204a355
danka74 <daniel.karlsson@socialstyrelsen.se>
2018-07-18 13:12:18 +0200

    added release notes v1

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const pug = require('pug');
-const bodyParser = require('body-parser');
 const path = require('path');
 const mysql = require('mysql');
 const util = require('util');
@@ -23,11 +22,50 @@ app.get('/', (req, res) => {
       throw err;
     }
 
+    results = results
+      .filter(db => db.Database.startsWith('snomed_full'))
+      .map(db => db.Database.substr(db.Database.lastIndexOf('_') + 1));
+
     res.render('selectDB', {
       results: results
     });
   })
 });
+
+app.get('/check-release', (req, res) => {
+  if (!req.query.includes('release')) {
+    throw 'no release selected';
+  }
+
+  const releaseConnection = mysql.createConnection({
+    host: '10.3.24.7',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: req.query['release']
+  });
+
+  releaseConnection.query(`
+    SELECT hierarchies.term, count(concepts.id) AS ct FROM concepts
+      JOIN transitiveclosure ON concepts.id = transitiveclosure.subtypeId
+      JOIN hierarchies ON transitiveclosure.supertypeId = hierarchies.conceptId
+    WHERE active = 1
+      AND moduleId = 45991000052106
+      AND effectiveTime = 20180531
+      AND id IN (SELECT id FROM snomed_full_SE1000052_20180531.concepts GROUP BY id HAVING count(*) = 1)
+    GROUP BY hierarchies.conceptId, hierarchies.term
+    ORDER BY hierarchies.displayOrder;`, (err, results, fields) => {
+
+      if(err) {
+        throw err;
+      }
+
+      res.render('genReleaseNotes', {
+        release: req.query['release'],
+        results: results
+      });
+  });
+
+})
 
 app.listen(3000, () => {
   console.log("Listening...");
