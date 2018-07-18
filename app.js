9251480b918ce8bb01bfd3f381e215a30864b665
danka74 <daniel.karlsson@socialstyrelsen.se>
2018-07-18 14:03:00 +0200

    refactoring to allow different queries

--- app.js
+++ app.js
@@ -1,32 +1,81 @@
-const express = require('express');
-const pug = require('pug');
-const path = require('path');
-const mysql = require('mysql');
-const util = require('util');
+const express = require("express");
+const pug = require("pug");
+const path = require("path");
+const mysql = require("mysql");
+const util = require("util");
 
 const app = express();
 
-app.set('views', path.join(__dirname, './views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "pug");
 
-app.get('/', (req, res) => {
+const queries = [
+  {
+    path: "new-concepts",
+    sql: `
+  SELECT hierarchies.term, count(concepts.id) AS ct FROM concepts
+    JOIN transitiveclosure ON concepts.id = transitiveclosure.subtypeId
+    JOIN hierarchies ON transitiveclosure.supertypeId = hierarchies.conceptId
+  WHERE active = 1
+    AND moduleId = 45991000052106
+    AND effectiveTime = $$release$$
+    AND id IN (SELECT id FROM snomed_full_SE1000052_20180531.concepts GROUP BY id HAVING count(*) = 1)
+  GROUP BY hierarchies.conceptId, hierarchies.term
+  ORDER BY hierarchies.displayOrder;
+  `
+  }
+];
+
+queries.forEach(qy => {
+  app.get(qy.path, (req, res) => {
+    if (!req.query["release"]) {
+      throw "no release selected";
+    }
+
+    const release = req.query["release"];
+
+    const releaseConnection = mysql.createConnection({
+      host: "10.3.24.7",
+      user: "root",
+      password: process.env.MYSQL_PASSWORD,
+      database: "snomed_full_SE1000052_" + release
+    });
+
+    const query = qy.sql.replace('$$release$$', release);
+
+    releaseConnection.query(query, (err, results, fields) => {
+      if (err) {
+        throw err;
+      }
+  
+      res.render(qy.path, {
+        release: release,
+        results: results
+      });
+    });
+  
+    releaseConnection.end();
+  });
+});
+
+app.get("/", (req, res) => {
   const startConnection = mysql.createConnection({
-    host: '10.3.24.7',
-    user: 'root',
+    host: "10.3.24.7",
+    user: "root",
     password: process.env.MYSQL_PASSWORD,
-    database: 'information_schema'
+    database: "information_schema"
   });
 
-  startConnection.query('show databases;', (err, results, fields) => {
-    if(err) {
+  startConnection.query("show databases;", (err, results, fields) => {
+    if (err) {
       throw err;
     }
 
     results = results
-      .filter(db => db.Database.startsWith('snomed_full'))
-      .map(db => db.Database.substr(db.Database.lastIndexOf('_') + 1));
+      .filter(db => db.Database.startsWith("snomed_full"))
+      .map(db => db.Database.substr(db.Database.lastIndexOf("_") + 1));
 
-    res.render('selectDB', {
+    res.render("select-db", {
       results: results
     });
   });
@@ -34,48 +83,49 @@ app.get('/', (req, res) => {
   startConnection.end();
 });
 
-app.get('/check-release', (req, res) => {
-  if (!req.query['release']) {
-    throw 'no release selected';
+app.get("/check-release", (req, res) => {
+  if (!req.query["release"]) {
+    throw "no release selected";
   }
 
-  const release = req.query['release'];
+  const release = req.query["release"];
 
   const releaseConnection = mysql.createConnection({
-    host: '10.3.24.7',
-    user: 'root',
+    host: "10.3.24.7",
+    user: "root",
     password: process.env.MYSQL_PASSWORD,
-    database: 'snomed_full_SE1000052_' + release
+    database: "snomed_full_SE1000052_" + release
   });
 
-  const query = `
+  const query =
+    `
   SELECT hierarchies.term, count(concepts.id) AS ct FROM concepts
     JOIN transitiveclosure ON concepts.id = transitiveclosure.subtypeId
     JOIN hierarchies ON transitiveclosure.supertypeId = hierarchies.conceptId
   WHERE active = 1
     AND moduleId = 45991000052106
-    AND effectiveTime = ` + release + `
+    AND effectiveTime = ` +
+    release +
+    `
     AND id IN (SELECT id FROM snomed_full_SE1000052_20180531.concepts GROUP BY id HAVING count(*) = 1)
   GROUP BY hierarchies.conceptId, hierarchies.term
   ORDER BY hierarchies.displayOrder;
   `;
 
   releaseConnection.query(query, (err, results, fields) => {
+    if (err) {
+      throw err;
+    }
 
-      if(err) {
-        throw err;
-      }
-
-      res.render('genReleaseNotes', {
-        release: req.query['release'],
-        results: results
-      });
+    res.render("genReleaseNotes", {
+      release: req.query["release"],
+      results: results
+    });
   });
 
   releaseConnection.end();
-
-})
+});
 
 app.listen(3000, () => {
   console.log("Listening...");
-})
+});
