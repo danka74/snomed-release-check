1644345a26eddb98e9fab291bce60ebe2770fe44
danka74 <daniel.karlsson@socialstyrelsen.se>
2018-07-19 14:27:23 +0200

    refactoring and more a refset query

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-//const pug = require("pug");
+const pug = require("pug");
 const path = require("path");
 const mysql = require("mysql");
 
@@ -8,119 +8,55 @@ const app = express();
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "pug");
 
-const queries = [
-  {
-    id: 'new-concepts',
-    description: 'Nya begrepp i releasen',
-    sql: `
-    SELECT hierarchies.term, count(concepts.id) AS ct FROM concepts
-      JOIN transitiveclosure ON concepts.id = transitiveclosure.subtypeId
-      JOIN hierarchies ON transitiveclosure.supertypeId = hierarchies.conceptId
-    WHERE active = 1
-      AND moduleId = 45991000052106
-      AND effectiveTime = __release__
-      AND id IN (SELECT id FROM concepts GROUP BY id HAVING count(*) = 1)
-    GROUP BY hierarchies.conceptId, hierarchies.term
-    ORDER BY hierarchies.displayOrder;`
-  },
-  {
-    id: 'changed-concepts',
-    description: 'Ändrade begrepp sedan förra releasen',
-    sql: `
-    SELECT 'changed', count(*) AS ct FROM concepts
-    WHERE moduleId = 45991000052106
-      AND effectiveTime = __release__
-      AND id IN (SELECT id FROM concepts GROUP BY id HAVING count(*) > 1)
-    UNION
-    SELECT 'inactivated', count(*) AS ct FROM concepts
-    WHERE moduleId = 45991000052106
-      AND effectiveTime = __release__
-      AND active = 0
-      AND id IN (SELECT id FROM concepts GROUP BY id HAVING count(*) > 1)`
-  },
-  {
-    id: 'descriptions',
-    description: 'Nya och borttagna beskrivningar',
-    sql: `
-    SELECT 'new translations', count(DISTINCT descriptions.id) AS ct FROM descriptions
-      JOIN concepts ON descriptions.conceptId = concepts.id
-    WHERE concepts.moduleId <> 45991000052106
-      AND descriptions.moduleId = 45991000052106
-      AND descriptions.effectiveTime = __release__
-      AND descriptions.languageCode = "sv"
-      AND descriptions.active = 1
-    UNION
-    SELECT 'new synonyms', count(*) AS ct FROM descriptions
-    WHERE active = 1
-      AND effectiveTime = __release__
-      AND conceptId IN (
-        SELECT conceptId FROM descriptions
-        WHERE active = 1
-          AND languageCode = "sv"
-        GROUP BY conceptId
-        HAVING count(id) > 1
-    )
-    UNION
-    SELECT 'inactivated descriptions', count(*) AS ct FROM descriptions
-    WHERE moduleId = 45991000052106
-      AND effectiveTime = __release__
-      AND languageCode = "sv"
-      AND active = 0;`
-  },
-  {
-    id: 'non-translated',
-    description: 'Icke-översatta begrepp efter hierarki',
-    sql: `
-    SELECT hierarchies.term, count(concepts_snap.id) AS ct FROM concepts_snap
-      JOIN transitiveclosure ON concepts_snap.id = transitiveclosure.subtypeId
-      JOIN hierarchies ON transitiveclosure.supertypeId = hierarchies.conceptId
-    WHERE active = 1 
-      AND Id NOT IN (SELECT conceptId FROM descriptions_snap WHERE active = 1 AND languageCode = "sv")
-    GROUP BY hierarchies.conceptId, hierarchies.term
-    ORDER BY hierarchies.displayOrder;`
+const queries = require("./queries");
+
+app.get("/query/:id/:release", (req, res) => {
+  if (!req.params["release"] || !req.params["id"]) {
+    throw "missing parameters";
   }
-];
 
-  app.get('/query/:id/:release', (req, res) => {
-    if (!req.params['release'] || !req.params['id']) {
-      throw 'missing parameters';
+  const release = req.params["release"];
+  const id = req.params["id"];
+
+  const query = queries.find(e => {
+    return e.id === id;
+  });
+
+  if (!query) {
+    res.sendStatus(404);
+    return;
+  }
+
+  const sqlQuery = query.sql.replace(/__release__/g, release);
+  //console.log(query);
+
+  const releaseConnection = mysql.createConnection({
+    host: "10.3.24.7",
+    user: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "snomed_full_SE1000052_" + release // TODO: here the Swedish extension ID is hardcoded!
+  });
+
+  releaseConnection.query(sqlQuery, (err, results, fields) => {
+    if (err) {
+      throw err;
     }
-    
-    const release = req.params['release'];
-    const id = req.params['id'];
 
-    var query = queries.find(e => {
-      return e.id === id;
-    });
-    if(!query) {
-      res.sendStatus(404);
-      return;
-    }
-
-    query = query.sql.replace(/__release__/g, release);
-    //console.log(query);
-
-    const releaseConnection = mysql.createConnection({
-      host: "10.3.24.7",
-      user: "root",
-      password: process.env.MYSQL_PASSWORD,
-      database: "snomed_full_SE1000052_" + release
-    });
-
-    releaseConnection.query(query, (err, results, fields) => {
-      if (err) {
-        throw err;
-      }
-  
+    if (query.pug) {
+      res.send(pug.render(query.pug, {
+        release: release,
+        results: results
+      }));
+    } else {
       res.render(id, {
         release: release,
         results: results
       });
-    });
-  
-    releaseConnection.end();
+    }
   });
 
+  releaseConnection.end();
+});
 
 app.get("/", (req, res) => {
   const startConnection = mysql.createConnection({
@@ -148,16 +84,29 @@ app.get("/", (req, res) => {
 });
 
 app.get("/check-release/:release", (req, res) => {
-  if (!req.params["release"]) {
+  const release = req.params["release"];
+
+  if (!release) {
     throw "no release selected";
   }
 
-  const release = req.params["release"];
-
-  res.render('queries', {
+  res.render("queries", {
     queries: queries,
     release: release
   });
+});
+
+app.get("/test1", (req, res) => {
+  res.send(
+    pug.render(
+      `html
+    body
+      h1 #{test1}`,
+      {
+        test1: "hejsan"
+      }
+    )
+  );
 });
 
 app.listen(3000, () => {
---
+++ queries.js
@@ -0,0 +1,105 @@
+const queries = [
+    {
+      id: 'new-concepts',
+      description: 'Nya begrepp i releasen',
+      sql: `
+      SELECT hierarchies.term, count(concepts.id) AS ct FROM concepts
+        JOIN transitiveclosure ON concepts.id = transitiveclosure.subtypeId
+        JOIN hierarchies ON transitiveclosure.supertypeId = hierarchies.conceptId
+      WHERE active = 1
+        AND moduleId = 45991000052106
+        AND effectiveTime = __release__
+        AND id IN (SELECT id FROM concepts GROUP BY id HAVING count(*) = 1)
+      GROUP BY hierarchies.conceptId, hierarchies.term
+      ORDER BY hierarchies.displayOrder;`
+    },
+    {
+      id: 'changed-concepts',
+      description: 'Ändrade begrepp sedan förra releasen',
+      sql: `
+      SELECT 'changed', count(*) AS ct FROM concepts
+      WHERE moduleId = 45991000052106
+        AND effectiveTime = __release__
+        AND id IN (SELECT id FROM concepts GROUP BY id HAVING count(*) > 1)
+      UNION
+      SELECT 'inactivated', count(*) AS ct FROM concepts
+      WHERE moduleId = 45991000052106
+        AND effectiveTime = __release__
+        AND active = 0
+        AND id IN (SELECT id FROM concepts GROUP BY id HAVING count(*) > 1)`
+    },
+    {
+      id: 'descriptions',
+      description: 'Nya och borttagna beskrivningar',
+      sql: `
+      SELECT 'new translations', count(DISTINCT descriptions.id) AS ct FROM descriptions
+        JOIN concepts ON descriptions.conceptId = concepts.id
+      WHERE concepts.moduleId <> 45991000052106
+        AND descriptions.moduleId = 45991000052106
+        AND descriptions.effectiveTime = __release__
+        AND descriptions.languageCode = 'sv'
+        AND descriptions.active = 1
+      UNION
+      SELECT 'new synonyms', count(*) AS ct FROM descriptions
+      WHERE active = 1
+        AND effectiveTime = __release__
+        AND conceptId IN (
+          SELECT conceptId FROM descriptions
+          WHERE active = 1
+            AND languageCode = 'sv'
+          GROUP BY conceptId
+          HAVING count(id) > 1
+      )
+      UNION
+      SELECT 'inactivated descriptions', count(*) AS ct FROM descriptions
+      WHERE moduleId = 45991000052106
+        AND effectiveTime = __release__
+        AND languageCode = 'sv'
+        AND active = 0;`
+    },
+    {
+      id: 'non-translated',
+      description: 'Icke-översatta begrepp efter hierarki',
+      sql: `
+      SELECT hierarchies.term, count(concepts_snap.id) AS ct FROM concepts_snap
+        JOIN transitiveclosure ON concepts_snap.id = transitiveclosure.subtypeId
+        JOIN hierarchies ON transitiveclosure.supertypeId = hierarchies.conceptId
+      WHERE active = 1 
+        AND Id NOT IN (SELECT conceptId FROM descriptions_snap WHERE active = 1 AND languageCode = 'sv')
+      GROUP BY hierarchies.conceptId, hierarchies.term
+      ORDER BY hierarchies.displayOrder;`
+    },
+    {
+      id: 'refsets-with-inactive',
+      description: 'Refsets med inaktiva begrepp',
+      sql: `
+      SELECT simplerefsets_snap.refsetId, descriptions_snap.term, count(*) FROM simplerefsets_snap
+        JOIN concepts_snap ON simplerefsets_snap.referencedComponentId = concepts_snap.id
+        JOIN descriptions_snap ON simplerefsets_snap.refsetId = descriptions_snap.conceptId
+        JOIN languagerefsets_snap ON descriptions_snap.id = languagerefsets_snap.referencedComponentId
+      WHERE simplerefsets_snap.active = 1
+        AND concepts_snap.active = 0
+        AND languagerefsets_snap.acceptabilityId = 900000000000548007
+      GROUP BY simplerefsets_snap.refsetId, descriptions_snap.term;`,
+      pug: `html
+      head
+        title Snomed release #{release} - Refsets med inaktiva begrepp
+      body
+        h1 Refsets med inaktiva begrepp
+        if results.length
+            table
+                th 
+                td RefsetId
+                td Namn
+                td Antal inaktiva begrepp
+                for refset in results
+                    tr
+                    td refset.id
+                    td refset.term
+                    td refset.ct
+        else
+            p Inga refset med inaktiva begrepp`
+    }
+  ];
+
+module.exports = queries;
